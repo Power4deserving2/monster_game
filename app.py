4fc042e24a775ffcea669a1ea6ccf15d8016fe19
Immanuel <ipmaster40@gmail.com>
2025-06-15 20:00:06 +0300

    feat(models): add Battle model to track wild and PvP battles

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from base import Base
 from models.players import Player
+from models.battles import Battle
 
 if __name__ =="__main__":
     engine =create_engine("sqlite:///Tables/monster.db")
